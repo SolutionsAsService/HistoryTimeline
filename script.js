1a74f023ec562a13c50864d50faf8c1e17f51b7a
Noah B <152567394+SolutionsAsService@users.noreply.github.com>
2025-01-30 22:41:36 -0800

    spacing

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ async function loadTimeline() {
             eventBar.style.width = `${widthPercent}%`;
 
             // Position based on event type
-            const topOffset = eventTypes[event.eventType] * 50; // space out by 50px
+            const topOffset = eventTypes[event.eventType] * 40; // space out by 50px
             eventBar.style.top = `${topOffset}px`;
 
             // Add label
