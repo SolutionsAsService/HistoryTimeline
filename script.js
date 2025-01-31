016eb7c6723b1148a3637d04c64d20a1f957a3dd
Noah B <152567394+SolutionsAsService@users.noreply.github.com>
2025-01-30 22:39:09 -0800

    fix

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ async function loadTimeline() {
             eventBar.style.width = `${widthPercent}%`;
 
             // Position based on event type
-            const topOffset = eventTypes[event.eventType] * 60; // space out by 60px
+            const topOffset = eventTypes[event.eventType] * 50; // space out by 50px
             eventBar.style.top = `${topOffset}px`;
 
             // Add label
